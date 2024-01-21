f34985e9adbba03cfa62dc9ff99a1b4a08987c4f
Mohammed Abid Nafi <abid.nafi04@gmail.com>
2024-01-21 18:49:28 +0530

    fix

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,5 +1,5 @@
 import Profile from "../../components/home/profile";
-import { Link, Stack } from "expo-router";
+import { Link, Stack, router } from "expo-router";
 import { Pressable, SafeAreaView, ScrollView, Text } from "react-native";
 import SearchDev from "../searchDev";
 import React, { useState } from "react";
@@ -12,29 +12,72 @@ import {
   Nunito_700Bold,
 } from "@expo-google-fonts/nunito";
 import AskQues from "../askQues";
+import { supabase } from "../../utils/supabase";
 
 const Home = () => {
+  const [name, setName] = useState("");
+  const [userid, setUserID] = useState("");
   const [fontsLoaded] = useFonts({
     Nunito_400Regular,
     Nunito_500Medium,
     Nunito_600SemiBold,
     Nunito_700Bold,
   });
-  const [data, setData] = useState("");
 
   if (!fontsLoaded) {
     return null;
+  }
+
+  async function getData() {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+
+    setUserID(user.id);
+  }
+  getData();
+
+  async function getName() {
+    const { data, error } = await supabase
+      .from("users")
+      .select("name")
+      .eq("uid", userid);
+
+    const jsonString = JSON.stringify(data);
+    const regexResult = /"name":"([^"]+)"/.exec(jsonString);
+    const extractedName = regexResult ? regexResult[1] : null;
+
+    if (extractedName) {
+      setName(extractedName);
+    }
+  }
+
+  getName();
+
+  function goAI() {
+    router.push("/ai");
   }
   return (
     <SafeAreaView className="bg-[#120126] pt-5 h-screen">
       <ScrollView className="">
         <Stack.Screen options={{ title: "Home", headerShown: true }} />
         <Text
-          className="text-white pb-5 px-2 text-lg"
+          className="text-white pb-5 px-2 text-lg mt-4"
           style={{ fontFamily: "Nunito_700Bold" }}
         >
-          Hi, Name
+          Hi, {name}
         </Text>
+        <Pressable
+          className="bg-violet-300 text-black text-lg h-14 w-15 p-4 w-fit mx-12 my-4 rounded-lg"
+          onPress={goAI}
+        >
+          <Text
+            className="text-black text-lg font-bold text-center"
+            style={{ fontFamily: "Nunito_700Bold" }}
+          >
+            Ask AI
+          </Text>
+        </Pressable>
         <SearchDev />
         <AskQues />
         <Pressable className="bg-violet-300 text-black text-lg h-14 w-15 p-4 w-fit mx-12 my-4 rounded-lg">
--- app/searchDev.tsx
+++ app/searchDev.tsx
@@ -11,14 +11,15 @@ import {
 } from "react-native";
 import { Icon } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { supabase } from "../utils/supabase";
 
 const search = require("../assets/search.png");
 
 const SearchDev = () => {
   return (
     <View className="flex flex-row space-x-2 m-2">
-      <View className="flex-row justify-center items-center mt-1 h-[50px]">
-        <View className="flex-1 h-full w-full rounded-l-md bg-violet-200 text-white  placeholder:text-white items-center justify-center">
+      <View className="w-full flex-row items-center mt-1 h-[50px] px-5">
+        <View className="h-full w-full rounded-l-md bg-violet-200 text-white  placeholder:text-white items-center justify-center">
           <TextInput
             className="font-regular w-full h-5 pl-4 placeholder:text-black"
             style={{ fontFamily: "Nunito_600SemiBold" }}
@@ -34,14 +35,6 @@ const SearchDev = () => {
           />
         </Pressable>
       </View>
-      <Pressable
-        className="bg-[#ffffff] rounded-xl h-12 w-10 flex items-center justify-center"
-        onPress={() => {
-          router.push("/settings");
-        }}
-      >
-        <Icon name="settings" size={30} color="#100" />
-      </Pressable>
     </View>
   );
 };
